92cfc1bc0898cf3a8e9d72feccd8b00bf2a02e72
Wes Basinger <wbasinger@villagetechschools.org>
2016-03-13 13:39:17 -0500

    Not all the way passing on part 2 but at least I got some of my points.

--- final/problem4part2.py
+++ final/problem4part2.py
@@ -9,14 +9,23 @@ def getSublists(L, n):
     return sublists
 
 def longestRun(L):
-    for slice_length in range(len(L), 0, -1):
-        all_slices = getSublists(L, slice_length)
-        for test_slice in all_slices:
-            if test_slice == range(test_slice[0], test_slice[slice_length-1]):
-                return len(test_slice)
-
-
-
+    longestRun = 1
+    all_lists = []
+    for i in range(1, len(L)):
+        all_lists.extend(getSublists(L, i))
+    for subset in all_lists:
+        current_run = 1
+        for i in range(len(subset)):
+            try:
+                if subset[i + 1] >= subset[i]:
+                    current_run +=1
+                else:
+                    current_run = 1
+            except IndexError:
+                break
+        if current_run > longestRun:
+            longestRun = current_run
+    return longestRun
 
 
 class TestCasesForFinalProblem4Part1(unittest.TestCase):
@@ -51,6 +60,35 @@ class TestCasesForFinalProblem4Part1(unittest.TestCase):
             ),
             5
         )
+        self.assertEqual(
+            longestRun([0]), 1
+        )
+        self.assertEqual(longestRun([1,1,1,1,1]), 5)
+
+        self.assertEqual(longestRun([-10, -5, 0, 5, 10]),
+            2
+        )
+        self.assertEqual(longestRun([10, 4, 6, 8, 3, 4, 5, 7, 7, 2]), 5)
+
+        self.assertEqual(longestRun([1, 2, 3, -1, -2, -3, -4, -5, -6]), 3)
+
+        self.assertEqual(longestRun([-1, -2, -3, -4, -5, -6, -7, 2, 3]), 3)
+
+        self.assertEqual(longestRun([1, 3, 5, -1, -3, -5, -7, 1, 3, 5])
+, 4)
+        self.assertEqual(longestRun([10, 8, 9, 5, 6, 7, 1, 2, 3, 4]),4)
+
+        self.assertEqual(longestRun([14, 16, 18, 20, 8, 10, 12, 4, 6, 2]), 4)
+
+        self.assertEqual(longestRun([7, 4, 1, -7, -11]), 1)
+
+        self.assertEqual(longestRun([10, 4, 6, 8, 3, 3, 4, 5, 7, 7, 2, 9])
+, 6)
+        self.assertEqual(longestRun([1, 0, 0, 0, 4, 5, 1, 2, 9, 4, -1, 0]), 5)
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
